833f331c70712fa02a7ca38d357c8487587e4956
IaraMoscovich <imosco2007@gmail.com>
2024-06-27 15:38:33 -0300

    termine la parte de la conexion de la IA y estoy haciendo la conexion desde la parte del back

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import Optional
 from typing import Union
 from fastapi import FastAPI
+from ultralytics import YOLO
 
 app = FastAPI()
-
+model = YOLO("modelo.pt")
 
 @app.get("/")
 async def read_root():
@@ -12,3 +13,9 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/image")
+#Aca los datos serian la direccion del .png a analizar
+async def ia(datos): #chequear que ia datos agarra el directorio de la imagen no la imagen en si
+    results = model(datos)
+    return {"resultado": results}
