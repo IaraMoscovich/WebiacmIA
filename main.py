11e852924c7cce1ab6c54004e0a39ca2160dfe4f
IaraMoscovich <imosco2007@gmail.com>
2024-07-04 09:09:53 -0300

    prueba
    para aprobar, preguntar a GAl

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from ultralytics import YOLO
 app = FastAPI()
 model = YOLO("modelo.pt")
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#async def read_item(item_id: int, q: Union[str, None] = None):
+  #  return {"item_id": item_id, "q": q}
 
-@app.get("/image")
+@app.get("/")
+async def read_root():
+    return {"Msg": "World"}
+
+#@app.get("/image")
 #Aca los datos serian la direccion del .png a analizar
-async def ia(datos): #chequear que ia datos agarra el directorio de la imagen no la imagen en si
-    results = model(datos)
-    return {"resultado": results}
+#async def ia(datos): #chequear que ia datos agarra el directorio de la imagen no la imagen en si
+ #   results = model(datos)
+  #  return {"resultado": results}
 
