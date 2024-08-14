9c88df72b18ef018ffb486f0b71e49df9e0e2472
IaraMoscovich <imosco2007@gmail.com>
2024-08-14 10:22:21 -0300

    nsds

--- API.py
+++ API.py
@@ -7,16 +7,6 @@ import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
-
 origins = ["*"]
 
 app.add_middleware(
