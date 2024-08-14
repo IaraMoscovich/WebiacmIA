6195f30a812efc066565ec98c32d9b42c394b5e4
IaraMoscovich <imosco2007@gmail.com>
2024-08-14 09:54:58 -0300

    a

--- API.py
+++ API.py
@@ -3,8 +3,19 @@ from fastapi.responses import JSONResponse
 import io
 from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
 origins = ["*"]
 
