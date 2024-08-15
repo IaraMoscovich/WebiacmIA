83ca90b53893dde1716bb4d23d64feaecdf5aea0
IaraMoscovich <imosco2007@gmail.com>
2024-08-15 15:44:28 -0300

    porrt

--- API.py
+++ API.py
@@ -46,4 +46,4 @@ async def upload_image(file: UploadFile = File(...)):
 
 import uvicorn
     #uvicorn.run(app, host="127.0.0.1", port=8000)
-uvicorn.run(app, host="0.0.0.0", port=10000, reload=True)
+uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
