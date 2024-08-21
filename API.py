b8aa7f4dbe3918f09404354c6aa2ce576d37cbf9
IaraMoscovich <110687618+IaraMoscovich@users.noreply.github.com>
2024-08-21 17:57:45 -0300

    a

--- API.py
+++ API.py
@@ -16,8 +16,8 @@ origins = ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
