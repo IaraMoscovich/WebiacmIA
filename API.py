76664db503b55a23db3b0ce7a35168fdd130a9a9
IaraMoscovich <imosco2007@gmail.com>
2024-09-03 11:18:49 -0300

    agregue panda dataframe para ver si puedo agarrar los resultados

--- API.py
+++ API.py
@@ -5,6 +5,7 @@ from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
 from ultralytics import YOLO
 import numpy as np
+import pandas as pd
 from supabase import create_client, Client
 
 app = FastAPI()
@@ -25,6 +26,20 @@ url: str = "https://afwgthjhqrgxizqydmvs.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmd2d0aGpocXJneGl6cXlkbXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4Nzg4OTUsImV4cCI6MjAzMTQ1NDg5NX0.Oq0wjvVrT8YJ4Q3q7Ji8-28qljja8h1sEBzZV5oXzzc"
 supabase: Client = create_client(url, key)
 
+def boxes_to_dataframe(boxes):
+    # Verificar si hay cajas detectadas
+    if boxes is None or len(boxes) == 0:
+        return pd.DataFrame(columns=['class'])
+
+    classes = boxes.cls.tolist()
+
+    # Crear un DataFrame
+    df = pd.DataFrame({
+        'class': classes
+    })
+
+    return df
+
 @app.post("/upload-image/")
 async def upload_image(file: UploadFile = File(...)):
     try:
@@ -33,16 +48,20 @@ async def upload_image(file: UploadFile = File(...)):
         image = Image.open(io.BytesIO(image_bytes))
         image_np = np.array(image)
         results = model.predict(image_np)
-       # return JSONResponse(content=results)
-      
+
+        # Convertir los resultados a un DataFrame
+        boxes_df = boxes_to_dataframe(results[0].boxes)
+        
+
+        # Convertir el DataFrame a un JSON serializable
+        boxes_json = boxes_df.to_json(orient='records')
+        print(boxes_json)
+
+        return JSONResponse(content={"detections": boxes_json})
+
     except Exception as e:
         print(e)
         return JSONResponse(content={"error": str(e)}, status_code=400)
-
-url: str ="https://afwgthjhqrgxizqydmvs.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmd2d0aGpocXJneGl6cXlkbXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4Nzg4OTUsImV4cCI6MjAzMTQ1NDg5NX0.Oq0wjvVrT8YJ4Q3q7Ji8-28qljja8h1sEBzZV5oXzzc"
-supabase: Client = create_client(url, key) 
-
 
 if __name__ == "__main__":
     import uvicorn
