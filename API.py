26296cc8e2d2cf205f4995b95804952373fb698b
IaraMoscovich <imosco2007@gmail.com>
2024-11-01 11:58:26 -0300

    a

--- API.py
+++ API.py
@@ -82,7 +82,7 @@ async def upload_image(file: UploadFile = File(...)):
         print(e)
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
