adff8f0dede133c6d4122950c419e8ef45dfa02b
IaraMoscovich <imosco2007@gmail.com>
2024-11-07 13:54:51 -0300

    agregue el if main

--- API.py
+++ API.py
@@ -65,3 +65,8 @@ async def upload_image(file: UploadFile = File(...)):
     except Exception as e:
         print("Error al procesar la imagen:", str(e))
         return JSONResponse(content={"error": str(e)}, status_code=400)
+
+
+    if __name__ == "__main__":
+        import uvicorn
+        uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
