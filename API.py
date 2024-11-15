49950ebd6c7dfd121353733156eea5fb40f482be
IaraMoscovich <110687618+IaraMoscovich@users.noreply.github.com>
2024-11-15 20:58:35 -0300

    nada

--- API.py
+++ API.py
@@ -16,7 +16,7 @@ torch.manual_seed(0)
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
