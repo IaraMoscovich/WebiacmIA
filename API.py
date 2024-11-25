4a85d6e97952ac04ca0595da57f46d563899298c
IaraMoscovich <110687618+IaraMoscovich@users.noreply.github.com>
2024-11-25 18:33:29 -0300

    a

--- API.py
+++ API.py
@@ -13,7 +13,7 @@ app = FastAPI()
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins= "https://webiacm-4rxkuqdb6-iaras-projects-d66e8430.vercel.app/",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
